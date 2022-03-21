2a7cb5a50f546ee4f6d165beb6d60aefb158dd54
Muhammad Asman <muhasmangit@gmail.com>
2022-03-21 10:29:45 +0700

    Membuat Animasi tulisan menggunakan library typed

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// ============================ typing animation =======================
+let typed = new Typed(".typing",{
+    strings:["","Web Designer","Web Developer","Front End Developer","Grapich Designer","Haha hihi"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
