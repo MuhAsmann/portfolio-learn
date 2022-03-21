41c7b3001c2239dcb46a8311901647c11b4c4c7b
Muhammad Asman <muhasmangit@gmail.com>
2022-03-21 13:46:30 +0700

    Menambahkan fungsi pemilih aktif pada side bar

--- js/script.js
+++ js/script.js
@@ -5,3 +5,24 @@ let typed = new Typed(".typing",{
     BackSpeed:60,
     loop:true
 })
+
+// ========================= aside ===========================
+const nav = document.querySelector(".nav"),
+    navList = nav.querySelectorAll("li"),
+    totalNavList = navList.length;
+
+    for(let i=0;i<totalNavList;i++){
+        //memilih tag a pada navlist
+        const a = navList[i].querySelector("a");
+        //ketika di klik jalankan fungsi
+        a.addEventListener("click", function(){
+            //sebelum itu jalankan perulangan untuk menghapus semua kelas active pada list
+            for(let j=0;j<totalNavList;j++){
+                navList[j].querySelector("a").classList.remove("active");
+            }
+            //tambahkan kelas active pada tag yang diklik
+            this.classList.add("active");
+            
+        });
+        //ulangi perulangan hingga akhir
+    }
